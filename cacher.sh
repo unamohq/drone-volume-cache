b795592948e5d8240e7d8d22cacc34842670e53b
Rafal Wojsznis <rafal.wojsznis@gmail.com>
2017-06-20 21:30:31 +0200

    Use single cache for all branches as it should fit better in our workflow

--- cacher.sh
+++ cacher.sh
@@ -7,9 +7,9 @@ if [ -z "$PLUGIN_MOUNT" ]; then
 fi
 
 if [[ $DRONE_COMMIT_MESSAGE == *"[CLEAR CACHE]"* && -n "$PLUGIN_RESTORE" && "$PLUGIN_RESTORE" == "true" ]]; then
-    if [ -d "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH" ]; then
+    if [ -d "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME" ]; then
         echo "Found [CLEAR CACHE] in commit message, clearing cache!"
-        rm -rf "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH"
+        rm -rf "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME"
     fi
 fi
 
@@ -23,21 +23,21 @@ if [[ -n "$PLUGIN_REBUILD" && "$PLUGIN_REBUILD" == "true" ]]; then
     # Create cache
     for source in "${SOURCES[@]}"; do
         if [ -d "$source" ]; then
-            echo "Rebuilding cache to /cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source from $source..."
-            mkdir -p "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source" && \
-                rsync -aHA --delete "$source/" "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source"
+            echo "Rebuilding cache to /cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source from $source..."
+            mkdir -p "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source" && \
+                rsync -aHA --delete "$source/" "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source"
         else
             echo "$source does not exist, removing from cached folder..."
-            rm -rf "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source"
+            rm -rf "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source"
         fi
     done
 elif [[ -n "$PLUGIN_RESTORE" && "$PLUGIN_RESTORE" == "true" ]]; then
     # Restore from cache
     for source in "${SOURCES[@]}"; do
-        if [ -d "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source" ]; then
-            echo "Restoring cache for $source from /cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source..."
+        if [ -d "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source" ]; then
+            echo "Restoring cache for $source from /cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source..."
             mkdir -p "$source" && \
-                rsync -aHA --delete "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$DRONE_COMMIT_BRANCH/$source/" "$source"
+                rsync -aHA --delete "/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME/$source/" "$source"
         else
             echo "No cache for $source"
         fi
